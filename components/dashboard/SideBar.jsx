551f90c41532dfd1e1ee9b5926d28bfb4602a67a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-08-31 17:32:57 +0600

    feat: add basic pages

--- components/dashboard/SideBar.jsx
+++ components/dashboard/SideBar.jsx
@@ -8,7 +8,7 @@ export default function SideBar() {
     return (
         <aside className="h-full py-4">
             <Card className="w-60 h-full p-4">
-                <Link href="#">
+                <Link href="/dashboard">
                     <p className="group flex space-x-2 items-center text-muted-foreground transition-colors hover:text-foreground">
                         <span className="hover:bg-primary hover:text-secondary w-8 h-8 flex items-center justify-center rounded-lg">
                             <HiOutlineHome />
