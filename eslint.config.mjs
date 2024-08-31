ba3511a74ff0abd55a31f554a9c6a5bf8f50e028
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-08-31 10:45:55 +0600

    feat: add prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,58 +4,58 @@ import pluginReact from 'eslint-plugin-react';
 import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default [
-  {
-    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'], // Lint JavaScript and TypeScript files
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      globals: {
-        ...globals.browser, // Use browser globals
-        jest: 'readonly', // Include Jest globals
-      },
-    },
-    plugins: {
-      react: pluginReact, // React plugin
-      jsxA11y: pluginJsxA11y, // Accessibility plugin
-    },
-    rules: {
-      // General JavaScript/ESLint rules
-      'no-console': ['warn', { allow: ['error', 'warn', 'info'] }],
-      'prefer-arrow-callback': ['error', { allowNamedFunctions: false }],
-      'prefer-const': 'error',
-      'no-var': 'error',
-      'object-shorthand': ['error', 'always'],
-      'prefer-template': 'error',
-      eqeqeq: ['error', 'always'],
-      semi: ['error', 'always'],
-      quotes: [
-        'error',
-        'single',
-        { avoidEscape: true, allowTemplateLiterals: true },
-      ],
-      'no-duplicate-imports': 'error',
-      'arrow-spacing': ['error', { before: true, after: true }],
-      'prefer-rest-params': 'error',
-      'prefer-spread': 'error',
-      'no-new-symbol': 'error',
-      'symbol-description': 'error',
-      'template-curly-spacing': 'error',
+    {
+        files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'], // Lint JavaScript and TypeScript files
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: {
+                ...globals.browser, // Use browser globals
+                jest: 'readonly', // Include Jest globals
+            },
+        },
+        plugins: {
+            react: pluginReact, // React plugin
+            jsxA11y: pluginJsxA11y, // Accessibility plugin
+        },
+        rules: {
+            // General JavaScript/ESLint rules
+            'no-console': ['warn', { allow: ['error', 'warn', 'info'] }],
+            'prefer-arrow-callback': ['error', { allowNamedFunctions: false }],
+            'prefer-const': 'error',
+            'no-var': 'error',
+            'object-shorthand': ['error', 'always'],
+            'prefer-template': 'error',
+            eqeqeq: ['error', 'always'],
+            semi: ['error', 'always'],
+            quotes: [
+                'error',
+                'single',
+                { avoidEscape: true, allowTemplateLiterals: true },
+            ],
+            'no-duplicate-imports': 'error',
+            'arrow-spacing': ['error', { before: true, after: true }],
+            'prefer-rest-params': 'error',
+            'prefer-spread': 'error',
+            'no-new-symbol': 'error',
+            'symbol-description': 'error',
+            'template-curly-spacing': 'error',
 
-      // React and JSX rules
-      'react/react-in-jsx-scope': 'off', // Not needed for Next.js since React 17+
-      'react/jsx-uses-react': 'off', // Not needed for Next.js since React 17+
-      'react/prop-types': 'off', // Disable prop-types as we're using TypeScript or prefer not to enforce it
-      'jsx-a11y/anchor-is-valid': 'off', // Disables warnings about invalid anchor tags for Next.js Link component
+            // React and JSX rules
+            'react/react-in-jsx-scope': 'off', // Not needed for Next.js since React 17+
+            'react/jsx-uses-react': 'off', // Not needed for Next.js since React 17+
+            'react/prop-types': 'off', // Disable prop-types as we're using TypeScript or prefer not to enforce it
+            'jsx-a11y/anchor-is-valid': 'off', // Disables warnings about invalid anchor tags for Next.js Link component
+        },
+        ignores: [
+            '.idea/**',
+            '.yarn/**',
+            'node_modules/**',
+            'build/**',
+            'logs/**',
+            'yarn.lock',
+            'documentation/**',
+            '.next/**',
+        ],
     },
-    ignores: [
-      '.idea/**',
-      '.yarn/**',
-      'node_modules/**',
-      'build/**',
-      'logs/**',
-      'yarn.lock',
-      'documentation/**',
-      '.next/**',
-    ],
-  },
 ];
