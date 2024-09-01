89aa43b7d1f550646bc284bab00ee02d8e4f9aa7
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-09-02 00:20:57 +0600

    feat: add environments API and dashboard

--- app/(dashboard)/environments/page.js
+++ app/(dashboard)/environments/page.js
@@ -11,114 +11,34 @@ import {
     TableHeader,
     TableRow,
 } from '@/components/ui/table';
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { GoCheck, GoEye, GoInfo, GoPencil, GoX } from 'react-icons/go';
+import RenderDialog from '@/components/dashboard/ActivityDescriptionDialog';
+import { GoCheck, GoPencil, GoX } from 'react-icons/go';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { deleteData, getData, createData, updateData } from '@/utilities/axios';
+import Spinner from '@/components/spinner/Spinner';
 
-import patternsConstants from '@/constants/environments.json';
-import { Textarea } from '@/components/ui/textarea';
-
-const renderRows = (
-    patterns,
-    editingState,
-    handleEditClick,
-    handleSaveClick,
-    handleCancelClick,
-    handleInputChange
-) => {
-    return patterns.map((pattern) => (
-        <TableRow key={pattern.id}>
-            <TableCell className="font-medium">{pattern.name}</TableCell>
-            <TableCell>
-                <Input
-                    className={
-                        !editingState[pattern.id] ? 'pointer-events-none' : ''
-                    }
-                    type="text"
-                    value={editingState[pattern.id]?.value || pattern.value}
-                    onChange={(e) =>
-                        handleInputChange(pattern.id, e.target.value)
-                    }
-                    readOnly={!editingState[pattern.id]?.isEditing}
-                />
-            </TableCell>
-            <TableCell className="text-right">
-                {editingState[pattern.id]?.isEditing ? (
-                    <>
-                        <Button
-                            variant="outline"
-                            onClick={() => handleSaveClick(pattern.id)}
-                        >
-                            <GoCheck />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            onClick={() => handleCancelClick(pattern.id)}
-                        >
-                            <GoX />
-                        </Button>
-                    </>
-                ) : (
-                    <>
-                        <AlertDialog>
-                            <AlertDialogTrigger>
-                                <GoInfo />
-                            </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogHeader>
-                                    <AlertDialogTitle>
-                                        Are you absolutely sure?
-                                    </AlertDialogTitle>
-                                    <AlertDialogDescription>
-                                        <Textarea
-                                            className="resize-y h-96"
-                                            placeholder={
-                                                editingState[pattern.id]
-                                                    ?.description ||
-                                                pattern.description
-                                            }
-                                        />
-                                    </AlertDialogDescription>
-                                </AlertDialogHeader>
-                                <AlertDialogFooter>
-                                    <AlertDialogCancel>
-                                        Cancel
-                                    </AlertDialogCancel>
-                                    {/*<AlertDialogAction>Continue</AlertDialogAction>*/}
-                                </AlertDialogFooter>
-                            </AlertDialogContent>
-                        </AlertDialog>
-
-                        <GoPencil
-                            className="cursor-pointer"
-                            onClick={() =>
-                                handleEditClick(pattern.id, pattern.value)
-                            }
-                        />
-                    </>
-                )}
-            </TableCell>
-        </TableRow>
-    ));
-};
-
-export default function Patterns() {
-    const [patterns, setPatterns] = useState([]);
+export default function Environments() {
+    const [environments, setEnvironments] = useState([]);
+    const [loading, setLoading] = useState(false);
     const [editingState, setEditingState] = useState({});
 
+    const fetchApiData = async () => {
+        try {
+            setLoading(true);
+
+            const data = await getData('/api/v1/dashboard/environments');
+
+            setEnvironments(data);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+        }
+    };
+
     useEffect(() => {
-        setPatterns(patternsConstants);
+        fetchApiData();
     }, []);
 
     const handleEditClick = (id, value) => {
@@ -128,36 +48,38 @@ export default function Patterns() {
         });
     };
 
+    const handleDeleteClick = async (id) => {
+        await deleteData('/api/v1/dashboard/environments/', id);
+
+        await fetchApiData();
+    };
+
     const handleSaveClick = async (id) => {
         const newValue = editingState[id].value;
+        const newName = editingState[id].name; // Assuming you also manage names
+        const newDescription = editingState[id].description; // Assuming descriptions are also editable
 
-        try {
-            const response = await fetch('/api/v1/dashboard/environments', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ id, value: newValue }),
-            });
+        await updateData(`/api/v1/dashboard/environments/${id}`, {
+            value: newValue,
+            name: newName,
+            description: newDescription,
+        });
 
-            if (response.ok) {
-                setPatterns((prevPatterns) =>
-                    prevPatterns.map((pattern) =>
-                        pattern.id === id
-                            ? { ...pattern, value: newValue }
-                            : pattern
-                    )
-                );
-                setEditingState({
-                    ...editingState,
-                    [id]: { isEditing: false },
-                });
-            } else {
-                console.error('Failed to update pattern.');
-            }
-        } catch (error) {
-            console.error('Failed to update pattern:', error);
-        }
+        await fetchApiData();
+    };
+
+    const handleResetToDefaultClick = async () => {
+        await createData(`/api/v1/dashboard/environments`, {});
+
+        await fetchApiData();
+    };
+
+    const handleDeleteAllClick = async () => {
+        await deleteData(`/api/v1/dashboard/environments`, '');
+
+        setEnvironments([]);
+
+        await fetchApiData();
     };
 
     const handleCancelClick = (id) => {
@@ -174,30 +96,137 @@ export default function Patterns() {
         });
     };
 
-    return (
+    return loading ? (
+        <Spinner />
+    ) : (
         <div>
-            <Table>
-                <TableCaption>
-                    A list of your used environments variables.
-                </TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableCell className="font-medium">Name</TableCell>
-                        <TableCell>Value</TableCell>
-                        <TableCell className="text-right">Edit</TableCell>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {renderRows(
-                        patterns,
-                        editingState,
-                        handleEditClick,
-                        handleSaveClick,
-                        handleCancelClick,
-                        handleInputChange
-                    )}
-                </TableBody>
-            </Table>
+            {environments ? (
+                <Table>
+                    <TableCaption>
+                        {environments.length
+                            ? 'A list of your used environments variables.'
+                            : 'No environments data found.'}
+                    </TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableCell className="font-medium">Name</TableCell>
+                            <TableCell>Value</TableCell>
+                            <TableCell className="text-right">Action</TableCell>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <RenderRows
+                            environments={environments}
+                            editingState={editingState}
+                            handleEditClick={handleEditClick}
+                            handleDeleteClick={handleDeleteClick}
+                            handleSaveClick={handleSaveClick}
+                            handleCancelClick={handleCancelClick}
+                            handleInputChange={handleInputChange}
+                        />
+                    </TableBody>
+                </Table>
+            ) : (
+                <p>No data </p>
+            )}
+
+            <div className="flex items-center gap-4">
+                <Button
+                    size="xs"
+                    className="text-xs p-1.5 px-2"
+                    onClick={() => handleResetToDefaultClick()}
+                >
+                    Reset to default
+                </Button>
+
+                <Button
+                    variant="destructive"
+                    size="xs"
+                    className="text-xs p-1.5 px-2"
+                    onClick={() => handleDeleteAllClick()}
+                >
+                    Delete All
+                </Button>
+            </div>
         </div>
     );
 }
+
+const RenderRows = ({
+    environments,
+    editingState,
+    handleEditClick,
+    handleDeleteClick,
+    handleSaveClick,
+    handleCancelClick,
+    handleInputChange,
+}) => {
+    return environments.map((environment) => (
+        <TableRow key={environment.id}>
+            <TableCell className="font-medium">{environment.name}</TableCell>
+            <TableCell>
+                {editingState[environment.id]?.isEditing ? (
+                    <Input
+                        className={
+                            !editingState[environment.id]
+                                ? 'pointer-events-none'
+                                : ''
+                        }
+                        type="text"
+                        value={
+                            editingState[environment.id]?.value ||
+                            environment.value
+                        }
+                        onChange={(e) =>
+                            handleInputChange(environment.id, e.target.value)
+                        }
+                        readOnly={!editingState[environment.id]?.isEditing}
+                    />
+                ) : (
+                    <p>{environment.value}</p>
+                )}
+            </TableCell>
+            <TableCell className="text-right">
+                {editingState[environment.id]?.isEditing ? (
+                    <>
+                        <Button
+                            variant="outline"
+                            onClick={() => handleSaveClick(environment.id)}
+                        >
+                            <GoCheck />
+                        </Button>
+                        <Button
+                            variant="outline"
+                            onClick={() => handleCancelClick(environment.id)}
+                        >
+                            <GoX />
+                        </Button>
+                    </>
+                ) : (
+                    <div className="flex items-center justify-evenly">
+                        <RenderDialog
+                            genderImage={environment}
+                            editingState={editingState}
+                            title="Are you absolutely sure?"
+                        />
+
+                        <GoPencil
+                            className="cursor-pointer text-blue-500"
+                            onClick={() =>
+                                handleEditClick(
+                                    environment.id,
+                                    environment.value
+                                )
+                            }
+                        />
+
+                        <AiOutlineDelete
+                            className="cursor-pointer text-destructive"
+                            onClick={() => handleDeleteClick(environment.id)}
+                        />
+                    </div>
+                )}
+            </TableCell>
+        </TableRow>
+    ));
+};
--- app/api/v1/dashboard/environments/route.js
+++ app/api/v1/dashboard/environments/route.js
@@ -1,48 +1,22 @@
-import fs from 'fs';
-import path from 'path';
-
-import httpStatus from '@/constants/httpStatus.constants';
-import configuration from '@/configuration/configuration';
-
-import sendResponse from '@/utilities/sendResponse';
-import getEnvironmentByName from '@/utilities/getEnvironmentByName';
+import service from '@/shared/service';
 
 export const POST = async (request) => {
-    const { id, value } = await request.json();
+    return service.createOrUpdateDefaults(
+        request,
+        'environments.json',
+        'environments',
+        'environments'
+    );
+};
 
-    const filePath = path.join(process.cwd(), 'constants/defaultUrls.json');
+export const GET = async (request) => {
+    return service.getValuesFromRedis(request, 'environments', 'environments');
+};
 
-    try {
-        // Read the existing JSON file
-        const fileData = fs.readFileSync(filePath, 'utf8');
-        const patterns = JSON.parse(fileData);
-
-        // Find the pattern by ID and update its value
-        const updatedUrls = patterns.map((url) =>
-            url.id === id ? { ...url, value } : url
-        );
-
-        // Write the updated data back to the JSON file
-        fs.writeFileSync(
-            filePath,
-            JSON.stringify(updatedUrls, null, 2),
-            'utf8'
-        );
-
-        return sendResponse(
-            request,
-            true,
-            httpStatus.OK,
-            'Url updated successfully'
-        );
-    } catch (error) {
-        return sendResponse(
-            request,
-            false,
-            httpStatus.INTERNAL_SERVER_ERROR,
-            configuration.env !== getEnvironmentByName('PRODUCTION')
-                ? error.message
-                : 'Internal Server Error.'
-        );
-    }
+export const DELETE = async (request) => {
+    return service.deleteValuesFromRedis(
+        request,
+        'environments',
+        'environments'
+    );
 };
