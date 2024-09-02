b432cd174211bc8cb8a7a8e99faa96b79fe47853
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-09-02 15:03:09 +0600

    refactor: remove unused code

--- app/api/v1/dashboard/content-types/[id]/route.js
+++ app/api/v1/dashboard/content-types/[id]/route.js
@@ -1,9 +1,4 @@
-import Redis from 'ioredis';
-
 import service from '@/shared/service';
-import configuration from '@/configuration/configuration';
-
-const redis = new Redis(configuration.redis.url);
 
 export const GET = async (request, context) => {
     return service.getValueByIdFromRedis(
