eb41c0b157ff6fe90b92e01dadca8849f5f63b9d
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-09-02 22:12:49 +0600

    feat: reuse RenderRows component

--- app/(dashboard)/constants/page.js
+++ app/(dashboard)/constants/page.js
@@ -7,17 +7,13 @@ import {
     TableBody,
     TableCaption,
     TableCell,
-    TableHead,
     TableHeader,
     TableRow,
 } from '@/components/ui/table';
-import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import RenderDialog from '@/components/dashboard/ActivityDescriptionDialog';
-import { GoCheck, GoPencil, GoX } from 'react-icons/go';
-import { AiOutlineDelete } from 'react-icons/ai';
 import { deleteData, getData, createData, updateData } from '@/utilities/axios';
 import Spinner from '@/components/spinner/Spinner';
+import RenderRows from '@/components/dashboard/RenderRows';
 
 export default function Constants() {
     const [constants, setConstants] = useState([]);
@@ -151,78 +147,3 @@ export default function Constants() {
         </div>
     );
 }
-
-const RenderRows = ({
-    constants,
-    editingState,
-    handleEditClick,
-    handleDeleteClick,
-    handleSaveClick,
-    handleCancelClick,
-    handleInputChange,
-}) => {
-    return constants.map((constant) => (
-        <TableRow key={constant.id}>
-            <TableCell className="font-medium">{constant.name}</TableCell>
-            <TableCell>
-                {editingState[constant.id]?.isEditing ? (
-                    <Input
-                        className={
-                            !editingState[constant.id]
-                                ? 'pointer-events-none'
-                                : ''
-                        }
-                        type="text"
-                        value={
-                            editingState[constant.id]?.value || constant.value
-                        }
-                        onChange={(e) =>
-                            handleInputChange(constant.id, e.target.value)
-                        }
-                        readOnly={!editingState[constant.id]?.isEditing}
-                    />
-                ) : (
-                    <p>{constant.value}</p>
-                )}
-            </TableCell>
-            <TableCell className="text-right">
-                {editingState[constant.id]?.isEditing ? (
-                    <>
-                        <Button
-                            variant="outline"
-                            onClick={() => handleSaveClick(constant.id)}
-                        >
-                            <GoCheck />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            onClick={() => handleCancelClick(constant.id)}
-                        >
-                            <GoX />
-                        </Button>
-                    </>
-                ) : (
-                    <div className="flex items-center justify-evenly">
-                        <RenderDialog
-                            activity={constant}
-                            editingState={editingState}
-                            title="Are you absolutely sure?"
-                        />
-
-                        <GoPencil
-                            className="cursor-pointer text-blue-500"
-                            onClick={() =>
-                                handleEditClick(constant.id, constant.value)
-                            }
-                        />
-
-                        <AiOutlineDelete
-                            className="cursor-pointer text-destructive"
-                            onClick={() => handleDeleteClick(constant.id)}
-                        />
-                    </div>
-                )}
-            </TableCell>
-        </TableRow>
-    ));
-};
