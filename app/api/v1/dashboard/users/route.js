153e2186e710022031240dd55b65a388527ea8e9
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-09-04 02:10:41 +0600

    feat: add users details

--- app/api/v1/dashboard/users/route.js
+++ app/api/v1/dashboard/users/route.js
@@ -1,17 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose, { model, models } from 'mongoose';
 
+import usersSchema from '@/app/api/v1/(users)/users.schema';
 import databaseService from '@/service/database.service';
 import httpStatus from '@/constants/httpStatus.constants.js';
 
 import sendResponse from '@/utilities/sendResponse.js';
 import sendErrorResponse from '@/utilities/sendErrorResponse';
+import getModelName from '@/utilities/getModelName';
 
 // Function to fetch documents from all collections using MongoDB aggregation
 async function fetchAllDocumentsFromAllCollections() {
     try {
-        console.debug('Connecting to database service');
-        await databaseService.connect();
-
         console.debug('Getting all collection names');
         const collections = await mongoose.connection.db
             .listCollections()
@@ -46,16 +45,61 @@ export async function GET(request) {
     console.debug('Fetching users from all collections');
 
     try {
-        const documentsFromAllCollections =
-            await fetchAllDocumentsFromAllCollections();
+        const url = new URL(request?.url);
+        const queryParams = Object.fromEntries(url.searchParams.entries());
 
-        return await sendResponse(
-            request,
-            true,
-            httpStatus.OK,
-            'Users fetched successfully from all collections.',
-            documentsFromAllCollections
-        );
+        console.debug('Connecting to database service');
+        await databaseService.connect();
+
+        if (queryParams.id) {
+            console.debug('Query Parameters:', queryParams);
+            console.debug(
+                `Retrieving user with ID: ${queryParams?.id} - ${queryParams?.siteName}`
+            );
+
+            const prepareModelName = getModelName(queryParams?.siteName);
+            if (!prepareModelName) {
+                return await sendResponse(
+                    request,
+                    false,
+                    httpStatus.BAD_REQUEST,
+                    'Invalid model name. Only alphabets are allowed without any spaces, hyphens, or special characters.'
+                );
+            }
+
+            const UsersModel =
+                (await models.prepareModelName) ||
+                model(prepareModelName, usersSchema);
+
+            const user = await UsersModel.findById(queryParams?.id).lean();
+            if (!user) {
+                return await sendResponse(
+                    request,
+                    false,
+                    httpStatus.NOT_FOUND,
+                    'No user found with the provided ID.'
+                );
+            }
+
+            return await sendResponse(
+                request,
+                true,
+                httpStatus.OK,
+                'Users fetched successfully.',
+                user
+            );
+        } else {
+            const documentsFromAllCollections =
+                await fetchAllDocumentsFromAllCollections();
+
+            return await sendResponse(
+                request,
+                true,
+                httpStatus.OK,
+                'Users fetched successfully from all collections.',
+                documentsFromAllCollections
+            );
+        }
     } catch (error) {
         return await sendErrorResponse(request, error);
     }
