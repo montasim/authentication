dbcac5424c0f4465a30cad4bd6d68bf75053790c
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-09-04 15:39:13 +0600

    refactor: fix duplicate increment for error

--- utilities/sendErrorResponse.js
+++ utilities/sendErrorResponse.js
@@ -1,18 +1,10 @@
-import databaseService from '@/service/database.service';
 import httpStatus from '@/constants/httpStatus.constants';
 import configuration from '@/configuration/configuration';
 
 import sendResponse from '@/utilities/sendResponse';
 import getEnvironmentByName from '@/utilities/getEnvironmentByName';
-import incrementUse from '@/utilities/incrementUse';
 
 const sendErrorResponse = async (request, error) => {
-    console.debug('Connecting to database service');
-    await databaseService.connect();
-
-    console.debug('Incrementing authentication module usage');
-    await incrementUse();
-
     return await sendResponse(
         request,
         false,
